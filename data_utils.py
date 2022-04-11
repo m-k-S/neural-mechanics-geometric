51f063bd99ddd2b13200a0b63d6ccb449eb90b11
Max Aalto <maxaalto@gmail.com>
2022-04-11 18:27:22 -0400

    adding data saving utilities

---
+++ data_utils.py
@@ -0,0 +1,70 @@
+import torch
+
+def initial_order_params(model, dataloader, loss, optimizer, device):
+    model.train()
+    for data in train_loader:  # Iterate in batches over the training dataset.
+        x = data.x.type(torch.FloatTensor).to(device)
+        edge_index = data.edge_index.to(device)
+        batch = data.batch.to(device)
+
+        out = model(x, edge_index, batch)  # Perform a single forward pass.
+
+        y = data.y.flatten().to(device)
+
+        loss = criterion(out, y)  # Compute the loss.
+        loss.backward()  # Derive gradients.
+
+        optimizer.zero_grad()  # Clear gradients.
+
+    activs = {}
+    grads = {}
+    a_idx = g_idx = 1
+    for mod in model.modules():
+        alayer = 'Layer {}'.format(a_idx)
+        glayer = 'Layer {}'.format(g_idx)
+        if isinstance(mod, Activs_prober):
+            if alayer not in activs:
+                activs[alayer] = mod.activs_norms
+            else:
+                activs[alayer] += mod.activs_norms
+            a_idx += 1
+        elif isinstance(mod, Conv_prober):
+            if glayer not in grads:
+                grads[glayer] = mod.grads_norms
+            else:
+                grads[glayer] +=  mod.grads_norms
+            g_idx += 1
+
+    activs = {k: torch.tensor(activs[k]).mean() for k in activs.keys()}
+    grads = {k: torch.tensor(grads[k]).mean() for k in grads.keys()}
+
+    return activs, grads
+
+def save_order_params(model):
+    activs = {}
+    grads = {}
+    a_idx = g_idx = 1
+    for mod in model.modules():
+        alayer = 'Layer {}'.format(a_idx)
+        glayer = 'Layer {}'.format(g_idx)
+        if isinstance(mod, Activs_prober):
+            if alayer not in activs:
+                activs[alayer] = mod.activs_norms
+            else:
+                activs[alayer] += mod.activs_norms
+            a_idx += 1
+        elif isinstance(mod, Conv_prober):
+            if glayer not in grads:
+                grads[glayer] = mod.grads_norms
+            else:
+                grads[glayer] +=  mod.grads_norms
+            g_idx += 1
+
+    return activs, grads
+
+def clear_order_params(model):
+    for mod in model.modules():
+        if isinstance(mod, Activs_prober):
+            mod.activs_norms = []
+        elif isinstance(mod, Conv_prober):
+            mod.grads_norms = []
--- order_parameters.py
+++ order_parameters.py
@@ -18,20 +18,23 @@ class Conv_prober(nn.Module):
 
             @staticmethod
             def backward(ctx, grad_output):
-                M = grad_output.view(grad_output.shape[0], -1)
-                self.grads_norms.append(M.norm().item())
+                if not self.training:
+                    return grad_output.clone(), None
+                else:
+                    M = grad_output.view(grad_output.shape[0], -1)
+                    self.grads_norms.append(M.norm().item())
 
-                # all_norms = torch.linalg.norm(M.type(torch.FloatTensor), dim=[1])
-                # num_nodes = self.batch.bincount()
-                # batch_size = self.batch.max() # zero indexed
-                # weighted_norm_sum_per_graph = []
-                # for b in range(batch_size):
-                #     b_norms = all_norms[self.batch == b].sum() / num_nodes[b]
-                #     weighted_norm_sum_per_graph.append(b_norms)
-                # norm_mean = sum(weighted_norm_sum_per_graph) / (batch_size+1)
-                # self.grads_norms.append(norm_mean.item())
+                    # all_norms = torch.linalg.norm(M.type(torch.FloatTensor), dim=[1])
+                    # num_nodes = self.batch.bincount()
+                    # batch_size = self.batch.max() # zero indexed
+                    # weighted_norm_sum_per_graph = []
+                    # for b in range(batch_size):
+                    #     b_norms = all_norms[self.batch == b].sum() / num_nodes[b]
+                    #     weighted_norm_sum_per_graph.append(b_norms)
+                    # norm_mean = sum(weighted_norm_sum_per_graph) / (batch_size+1)
+                    # self.grads_norms.append(norm_mean.item())
 
-                return grad_output.clone(), None
+                    return grad_output.clone(), None
 
         self.cal_prop = sim_grads.apply
 
@@ -60,32 +63,35 @@ class Activs_prober(nn.Module):
         class sim_activs(torch.autograd.Function):
             @staticmethod
             def forward(ctx, input, batch):
-                M = input.clone()
+                if not self.training:
+                    return input.clone()
+                else:
+                    M = input.clone()
 
-                # Activation Norm
-                all_norms = torch.linalg.norm(M.type(torch.FloatTensor), dim=[1])
-                num_nodes = batch.bincount()
-                batch_size = batch.max() # zero indexed
-                weighted_norm_sum_per_graph = []
-                for b in range(batch_size):
-                    b_norms = all_norms[batch == b].sum() / num_nodes[b]
-                    weighted_norm_sum_per_graph.append(b_norms)
-                norm_mean = sum(weighted_norm_sum_per_graph) / (batch_size+1)
-                self.activs_norms.append(norm_mean.item())
+                    # Activation Norm
+                    all_norms = torch.linalg.norm(M.type(torch.FloatTensor), dim=[1])
+                    num_nodes = batch.bincount()
+                    batch_size = batch.max() # zero indexed
+                    weighted_norm_sum_per_graph = []
+                    for b in range(batch_size):
+                        b_norms = all_norms[batch == b].sum() / num_nodes[b]
+                        weighted_norm_sum_per_graph.append(b_norms)
+                    norm_mean = sum(weighted_norm_sum_per_graph) / (batch_size+1)
+                    self.activs_norms.append(norm_mean.item())
 
-                # These functions need fixing to process graphs properly (i.e. weight by the number of nodes in a graph, using the batch input variable)
+                    # These functions need fixing to process graphs properly (i.e. weight by the number of nodes in a graph, using the batch input variable)
 
-                # Activation Correlations
-                # M = (M / anorms).reshape(M.shape[0], -1)
-                # M = torch.matmul(M, M.T)
-                # self.activs_corr.append(((M.sum(dim=1) - 1) / (M.shape[0]-1)).mean().item())
+                    # Activation Correlations
+                    # M = (M / anorms).reshape(M.shape[0], -1)
+                    # M = torch.matmul(M, M.T)
+                    # self.activs_corr.append(((M.sum(dim=1) - 1) / (M.shape[0]-1)).mean().item())
 
-                # Activation Ranks (calculates stable rank)
-                # tr = torch.diag(M).sum()
-                # opnom = torch.linalg.norm(M + 1e-8 * M.mean() * torch.rand(M.shape).to(self.device), ord=2)
-                # self.activs_ranks.append((tr / opnom).item())
+                    # Activation Ranks (calculates stable rank)
+                    # tr = torch.diag(M).sum()
+                    # opnom = torch.linalg.norm(M + 1e-8 * M.mean() * torch.rand(M.shape).to(self.device), ord=2)
+                    # self.activs_ranks.append((tr / opnom).item())
 
-                return input.clone()
+                    return input.clone()
 
             @staticmethod
             def backward(ctx, grad_output):
@@ -98,6 +104,3 @@ class Activs_prober(nn.Module):
             return input
         else:
             return self.cal_prop(input, batch)
-
-def test2():
-    print ('test')
