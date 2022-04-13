b1b133b3f6a350906efbde51acaa2dc2bd0aafb8
Max Aalto <maxaalto@gmail.com>
2022-04-13 17:33:38 -0400

    updating data saving methods

--- data_utils.py
+++ data_utils.py
@@ -1,7 +1,6 @@
 import torch
 from order_parameters import Activs_prober, Conv_prober
 
-
 def initial_order_params(model, dataloader, criterion, optimizer, device):
     model.train() # Set model to train for proper BatchNorm behavior
 
@@ -22,11 +21,15 @@ def initial_order_params(model, dataloader, criterion, optimizer, device):
 
     activs = {}
     grads = {}
+    ranks = {}
+
     for idx, mod in enumerate(model.activ_probes):
         if idx not in activs:
             activs[idx] = mod.activs_norms
+            ranks[idx] = mod.activs_ranks
         else:
             activs[idx] += mod.activs_norms
+            ranks[idx] += mod.activs_ranks
 
     for idx, mod in enumerate(model.conv_probes):
         if idx not in grads:
@@ -34,19 +37,25 @@ def initial_order_params(model, dataloader, criterion, optimizer, device):
         else:
             grads[idx] += mod.grads_norms
 
+    # Aggregate across a full training epoch
     activs = {k: torch.tensor(activs[k]).mean() for k in activs.keys()}
     grads = {k: torch.tensor(grads[k]).mean() for k in grads.keys()}
+    ranks = {k: torch.tensor(ranks[k]).mean() for k in ranks.keys()}
 
-    return activs, grads
+    return activs, grads, ranks
 
 def save_order_params(model):
     activs = {}
     grads = {}
+    ranks = {}
+
     for idx, mod in enumerate(model.activ_probes):
         if idx not in activs:
             activs[idx] = mod.activs_norms
+            ranks[idx] = mod.activs_ranks
         else:
             activs[idx] += mod.activs_norms
+            ranks[idx] += mod.activs_ranks
 
     for idx, mod in enumerate(model.conv_probes):
         if idx not in grads:
@@ -54,11 +63,12 @@ def save_order_params(model):
         else:
             grads[idx] += mod.grads_norms
 
-    return activs, grads
+    return activs, grads, ranks
 
 def clear_order_params(model):
     for idx, mod in enumerate(model.activ_probes):
         mod.activs_norms = []
+        mod.activs_ranks = []
 
     for idx, mod in enumerate(model.conv_probes):
         mod.grads_norms = []
