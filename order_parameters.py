324a8f6142e94317dd53bafe293dad5c702c71d7
Max Aalto <maxaalto@gmail.com>
2022-04-13 17:30:33 -0400

    addingjupyter notebook and gcn matrix rank computation

--- order_parameters.py
+++ order_parameters.py
@@ -83,9 +83,8 @@ class Activs_prober(nn.Module):
                     # self.activs_corr.append(((M.sum(dim=1) - 1) / (M.shape[0]-1)).mean().item())
 
                     # Activation Ranks (calculates stable rank)
-                    tr = torch.diag(M).sum()
-                    opnom = torch.linalg.norm(M + 1e-8 * M.mean() * torch.rand(M.shape).to(self.device), ord=2) # stabilize the matrix
-                    self.activs_ranks.append((tr / opnom).item())
+                    rank = torch.matrix_rank(M)
+                    self.activs_ranks.append(M.item())
 
                     return input.clone()
 
