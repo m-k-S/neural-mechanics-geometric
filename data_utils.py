65ac7d9f33c0c4480145129573720bbe9754382c
Max Aalto <maxaalto@gmail.com>
2022-04-25 12:55:29 -0400

    more order params

--- data_utils.py
+++ data_utils.py
@@ -20,36 +20,50 @@ def initial_order_params(model, dataloader, criterion, optimizer, device):
 
     activs = {}
     grads = {}
-    full_ranks = {}
-    graph_mean_ranks = {}
-    feature_ranks = {}
+    activs_full_ranks = {}
+    activs_graph_mean_ranks = {}
+    activs_feature_ranks = {}
+    conv_full_ranks = {}
+    conv_graph_mean_ranks = {}
+    conv_feature_ranks = {}
 
     for idx, mod in enumerate(model.activ_probes):
         if idx not in activs:
             activs[idx] = mod.activs_norms
-            full_ranks[idx] = mod.full_ranks
-            graph_mean_ranks[idx] = mod.graph_mean_ranks
-            feature_ranks[idx] = mod.feature_ranks
+            activs_full_ranks[idx] = mod.full_ranks
+            activs_graph_mean_ranks[idx] = mod.graph_mean_ranks
+            activs_feature_ranks[idx] = mod.feature_ranks
         else:
             activs[idx] += mod.activs_norms
-            full_ranks[idx] += mod.full_ranks
-            graph_mean_ranks[idx] += mod.graph_mean_ranks
-            feature_ranks[idx] += mod.feature_ranks
+            activs_full_ranks[idx] += mod.full_ranks
+            activs_graph_mean_ranks[idx] += mod.graph_mean_ranks
+            activs_feature_ranks[idx] += mod.feature_ranks
 
     for idx, mod in enumerate(model.conv_probes):
         if idx not in grads:
             grads[idx] = mod.grads_norms
+            conv_full_ranks[idx] = mod.full_ranks
+            conv_graph_mean_ranks[idx] = mod.graph_mean_ranks
+            conv_feature_ranks[idx] = mod.feature_ranks
         else:
             grads[idx] += mod.grads_norms
+            conv_full_ranks[idx] += mod.full_ranks
+            conv_graph_mean_ranks[idx] += mod.graph_mean_ranks
+            conv_feature_ranks[idx] += mod.feature_ranks
 
     # Aggregate across a full training epoch
     activs = {k: torch.tensor(activs[k]).mean() for k in activs.keys()}
     grads = {k: torch.tensor(grads[k]).mean() for k in grads.keys()}
-    full_ranks = {k: torch.tensor(full_ranks[k]).mean() for k in full_ranks.keys()}
-    graph_mean_ranks = {k: torch.tensor(graph_mean_ranks[k]).mean() for k in graph_mean_ranks.keys()}
-    feature_ranks = {k: [torch.tensor(feature_rank).mean() for feature_rank in feature_ranks[k]] for k in feature_ranks.keys()}
 
-    return activs, grads, full_ranks, graph_mean_ranks, feature_ranks
+    activs_full_ranks = {k: torch.tensor(activs_full_ranks[k]).mean() for k in activs_full_ranks.keys()}
+    activs_graph_mean_ranks = {k: torch.tensor(activs_graph_mean_ranks[k]).mean() for k in activs_graph_mean_ranks.keys()}
+    activs_feature_ranks = {k: [torch.tensor(feature_rank).mean() for feature_rank in activs_feature_ranks[k]] for k in activs_feature_ranks.keys()}
+
+    conv_full_ranks = {k: torch.tensor(conv_full_ranks[k]).mean() for k in conv_full_ranks.keys()}
+    conv_graph_mean_ranks = {k: torch.tensor(conv_graph_mean_ranks[k]).mean() for k in conv_graph_mean_ranks.keys()}
+    conv_feature_ranks = {k: [torch.tensor(feature_rank).mean() for feature_rank in conv_feature_ranks[k]] for k in feature_ranks.keys()}
+
+    return activs, grads, activs_full_ranks, activs_graph_mean_ranks, activs_feature_ranks, conv_full_ranks, conv_graph_mean_ranks, conv_feature_ranks
 
 def save_order_params(model):
     activs = {}
@@ -87,3 +101,6 @@ def clear_order_params(model):
 
     for idx, mod in enumerate(model.conv_probes):
         mod.grads_norms = []
+        mod.full_ranks = []
+        mod.graph_mean_ranks = []
+        mod.feature_ranks = []
