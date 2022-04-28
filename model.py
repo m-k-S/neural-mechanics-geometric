70095ee6612bd018a074e412113f3bafbd90ba13
Max Aalto <maxaalto@gmail.com>
2022-04-27 20:29:59 -0400

    node classification model

--- model.py
+++ model.py
@@ -70,3 +70,67 @@ class GraphNet(torch.nn.Module):
         x = self.lin(x)
 
         return x
+
+class NodeNet(torch.nn.Module):
+    def __init__(
+            self,
+            hidden_channels,
+            num_input_features,
+            num_classes,
+            num_layers=3,
+            conv_type="GCN",
+            norm=None,
+            norm_args=None):
+        # Normalization layers need specific kwargs:
+        # BatchNorm: in_channels (int)
+        # GraphNorm: in_channels (int)
+        # PairNorm: scale (int)
+        # DiffGroupNorm: in_channels (int), groups (int)
+        super(NodeNet, self).__init__()
+        self.num_layers = num_layers
+
+        conv_map = {'GCN': GCNConv, 'SAGE': SAGEConv, 'GraphConv': GraphConv, 'GAT': GATv2Conv}
+        self.conv_type = conv_type
+
+        norm_map = {'BatchNorm': BatchNorm, 'GraphNorm': GraphNorm, 'PairNorm': PairNorm, 'DiffGroupNorm': DiffGroupNorm}
+        self.norm = norm
+
+        self.conv_layers = nn.ModuleList()
+        self.conv_probes = nn.ModuleList()
+        self.norm_layers = nn.ModuleList()
+        self.activations = nn.ModuleList()
+        self.activ_probes = nn.ModuleList()
+
+        for l in range(num_layers):
+            if l == 0:
+                self.conv_layers.append(conv_map[conv_type](num_input_features, hidden_channels))
+            else if l == num_layers - 1:
+                self.conv_layers.append(conv_map[conv_type](hidden_channels, num_classes))
+                self.conv_probes.append(ConvolutionProbe())
+                continue
+            else:
+                self.conv_layers.append(conv_map[conv_type](hidden_channels, hidden_channels))
+
+            self.conv_probes.append(ConvolutionProbe())
+            if norm:
+                if norm_args:
+                    self.norm_layers.append(norm_map[norm](**norm_args))
+                else:
+                    self.norm_layers.append(norm_map[norm]())
+
+            self.activations.append(nn.ReLU(inplace=True))
+            self.activ_probes.append(ActivationProbe())
+
+    def forward(self, x, edge_index, batch):
+        for l in range(self.num_layers - 1):
+            x = self.conv_layers[l](x, edge_index)
+            x = self.conv_probes[l](x, batch)
+            if self.norm:
+                x = self.norm_layers[l](x)
+            x = self.activations[l](x)
+            x = self.activ_probes[l](x, batch)
+
+        x = self.conv_layers[-1](x, edge_index)
+        x = self.conv_probes[-1](x, batch)
+
+        return x
